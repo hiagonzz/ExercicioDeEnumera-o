c305cdb6f4c4600541bcfcb2774e96b67985cd00
Hiago nunes pimentel <derundaclash665@gmail.com>
2023-11-17 13:04:50 -0300

    finalizado o exercicio

--- src/entities/Post.java
+++ src/entities/Post.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import java.util.List;
 
 public class Post {
-    private  static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
     private Date moment;
     private String title;
     private String content;
@@ -66,20 +66,20 @@ public class Post {
     public void addComment(Comment comment) {
         comments.add(comment);
     }
-    public void  removeComment (Comment comment){
+
+    public void removeComment(Comment comment) {
         comments.remove(comment);
     }
 
-
-    public String  toString(){
-        StringBuilder sb = new  StringBuilder();
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
         sb.append(title + "\n");
         sb.append(like);
         sb.append("Likes - ");
         sb.append(sdf.format(moment) + "\n");
         sb.append(content + "\n");
         sb.append("Comments: \n");
-        for (Comment  c : comments) {
+        for (Comment c : comments) {
             sb.append(c.getText() + "\n");
         }
         return sb.toString();
