9a8b7cad5fd5b44eef5522b70ca3b4ca456b2148
Hiago nunes pimentel <derundaclash665@gmail.com>
2023-11-17 11:45:23 -0300

    adicionando a (comment)

--- src/entities/Comment.java
+++ src/entities/Comment.java
@@ -5,4 +5,16 @@ public class Comment {
 private String text;
 
 
+public Comment (String text){
+this.text = text;
+}
+
+
+public String getText() {
+    return text;
+}
+public void setText(String text) {
+    this.text = text;
+}
+
 }
